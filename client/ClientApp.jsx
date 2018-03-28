f69b4983dd4d2d4dfc4c85139e1219bc350801b7
ramusus <ramusus@gmail.com>
2018-03-27 20:26:16 -0400

    fix safari bug, update deps

--- client/ClientApp.jsx
+++ client/ClientApp.jsx
@@ -4,12 +4,13 @@ import { hot } from 'react-hot-loader'
 import { ApolloProvider } from 'react-apollo'
 import { BrowserRouter } from 'react-router-dom'
 import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-client-preset'
+import fetch from 'unfetch'
 
 import App from 'components/App/App'
 
 const client = new ApolloClient({
   ssrForceFetchDelay: 100,
-  link: new HttpLink({ uri: window.API_URL }),
+  link: new HttpLink({ uri: window.API_URL, fetch }),
   cache: new InMemoryCache().restore(window.INITIAL_STATE)
 })
 
