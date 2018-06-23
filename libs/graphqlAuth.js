9516f5bee9725f9ed45fa7224517b317ab33fa53
ramusus <ramusus@gmail.com>
2018-06-23 17:56:56 -0400

    remove token from request

--- libs/graphqlAuth.js
+++ libs/graphqlAuth.js
@@ -1,12 +1,13 @@
 // @flow
-import { setContext } from 'apollo-link-context';
+import { setContext } from 'apollo-link-context'
 
-export default setContext((_, { headers }) => {
-  // const token = '';
-  return {
-    headers: {
-      ...headers,
-      // authorization: token ? `JWT ${token}` : '',
-    }
-  }
-});
+export default setContext((_, { headers }) => ({ headers }))
+// export default setContext((_, { headers }) => {
+//   const token = '';
+//   return {
+//     headers: {
+//       ...headers,
+//       authorization: token ? `JWT ${token}` : '',
+//     }
+//   }
+// });
