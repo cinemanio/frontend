618ba07e36084fd0f2ce7bb0649c1527f1a459ba
ramusus <ramusus@gmail.com>
2018-08-30 19:14:47 -0400

    small and full version of wiki block

--- components/ObjectWikipedia/ObjectWikipedia.jsx
+++ components/ObjectWikipedia/ObjectWikipedia.jsx
@@ -7,10 +7,12 @@ import wtf from 'wtf_wikipedia'
 import Block from 'components/Block/Block'
 
 import WikiSection from './WikiSection/WikiSection'
+import './ObjectWikipedia.scss'
 
 type Props = { object: Object }
+type State = { full: boolean }
 
-export default class ObjectWikipedia extends React.PureComponent<Props> {
+export default class ObjectWikipedia extends React.PureComponent<Props, State> {
   static propTypes = {
     object: PropTypes.object.isRequired,
   }
@@ -46,17 +48,39 @@ export default class ObjectWikipedia extends React.PureComponent<Props> {
     `,
   }
 
-  getContent() {
+  constructor(props: Object) {
+    super(props)
+    this.state = { full: false }
+  }
+
+  get content(): string {
     const edges = this.props.object.wikipedia.edges.filter(edge => edge.node.lang === this.context.i18n.language)
-    return edges.length > 0 ? edges[0].node.content : null
+    let content = edges.length > 0 ? edges[0].node.content : null
+    if (content && !this.state.full) {
+      content = `${content.substring(0, 500)}…`
+    }
+    return content
+  }
+
+  displayFull = (e: Event) => {
+    e.preventDefault()
+    this.setState({ full: true })
+  }
+
+  renderLink() {
+    return this.state.full ? ''
+      : <a href="#" onClick={this.displayFull}>{this.context.i18n.t('wikipedia.displayFull')}</a>
   }
 
   render() {
-    const content = this.getContent()
+    const { content } = this
     return !content ? '' : (
-      <Block title={this.context.i18n.t('wikipedia.title')}>
-        {wtf(content).data.sections.map(section => <WikiSection section={section}/>)}
-      </Block>
+      <div styleName="wikipedia">
+        <Block title={this.context.i18n.t('wikipedia.title')}>
+          {wtf(content).data.sections.map(section => <WikiSection key={section} section={section}/>)}
+          {this.renderLink()}
+        </Block>
+      </div>
     )
   }
 }
