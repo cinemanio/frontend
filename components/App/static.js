885840548b4f67561c29ba18b6f66f2ee76e9ee0
ramusus <ramusus@gmail.com>
2018-11-15 23:20:00 -0400

    console.log

--- components/App/static.js
+++ components/App/static.js
@@ -3,6 +3,8 @@ import settings from 'settings'
 const extract = (regexp) => {
   // eslint-disable-next-line global-require
   const webpackStates = require('../../webpack-stats.json')
+  // eslint-disable-next-line
+  console.log(webpackStates)
   return webpackStates.status === 'done'
     ? webpackStates.chunks.app.map(chunk => chunk.publicPath).filter(path => path.match(regexp))
     : []
